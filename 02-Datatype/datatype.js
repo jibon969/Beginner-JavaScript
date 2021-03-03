6e5af73da6b4eceffeda7597aba4160ac9271d7d
Jibon Ahmed <jayed.swe@gmail.com>
2021-03-04 01:06:21 +0600

    added datatype

---
+++ 02-Datatype/datatype.js
@@ -0,0 +1,28 @@
+// Javascript has 4 datatype
+/*
+    1. string
+    2. number
+    3. boolean
+    4. undefined
+    5. null
+*/
+
+// Example of string
+var x = "Hello World"
+console.log(x);
+console.log(typeof (x));
+
+// Example of number
+var i = 10;
+console.log(i);
+console.log(typeof (i));
+
+// Example of boolean
+var x = true;
+console.log(x);
+console.log(typeof (x));
+
+// Example of null
+var x = null;
+console.log(x);
+console.log(typeof (x));
