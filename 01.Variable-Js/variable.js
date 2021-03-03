5987f47fa670684e153f8831eacb863cdaa732ff
Jibon Ahmed <jayed.swe@gmail.com>
2021-03-04 00:37:07 +0600

    added variable

---
+++ 01.Variable-Js/variable.js
@@ -0,0 +1,30 @@
+
+var planet = "Earth";
+console.log(planet);
+
+// More Example
+
+var valueOfPi = 3.1416
+console.log(valueOfPi);
+
+// Variable nameing
+/*
+    1. lowercase
+    2. UPPERCASE
+    3. CamelCase
+    4. camelCase 
+    best pratic lowercase or  camelCase
+*/
+
+// Example
+var myNumber = 12;
+myNumber = 13
+console.log(myNumber);
+myNumber = 14
+console.log(myNumber);
+
+// never change value
+const pi = 3.14159;
+console.log(pi);
+
+
